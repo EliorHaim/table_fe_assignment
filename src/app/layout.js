c84501d3f0d730cd87b1ab873f4f1e4440923262
eliorhaim <haimelior@gmail.com>
2025-01-03 09:56:59 +0200

    Moved Components to a new Components directory removed unused code

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,5 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import DarkModeToggle from './DarkModeToggle';
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import DarkModeToggle from '../Components/DarkModeToggle';
 
 export const metadata = {
   title: "Create Next App",
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 'use client';
-import Table from "./Table";
+import Table from "../Components/Table";
 
 export default function Home() {
   return (
